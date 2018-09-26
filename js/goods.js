aca0bbf4b2cce38432d5975d56860274231351a7
Nastasia <nastya.nty@gmail.com>
2018-09-26 23:49:13 +0500

    Fragment

--- js/goods.js
+++ js/goods.js
@@ -67,13 +67,18 @@ var nameArr = ['Чесночные сливки', 'Огуречный педан
     return contents
   }
 
-var getArr = function() {
+  var userDialog = document.querySelector('.catalog__cards');
+  userDialog.classList.remove('catalog__cards--load');
+  userDialog.querySelector('.catalog__load').classList.add('visually-hidden');
 
-  var arr = [];
+  var cardsTemplate = document.querySelector('#card')
+    .content
+    .querySelector('.catalog__card');
 
-  for(var i = 0; i < 26; i++) {
+  var getCards = function (){
+  var cardsElement = cardsTemplate.cloneNode(true);
 
-    arr[i] = {
+    var arr = {
       name: nameArr[randomInteger(0, nameArr.length - 1)],
       picture: pictureArr[randomInteger(0, pictureArr.length - 1)],
       amount: randomInteger(0,20),
@@ -83,16 +88,52 @@ var getArr = function() {
         value: randomInteger(1,5),
         number: randomInteger(10, 900)
       },
-      nutritionFacts: {
+        nutritionFacts: {
         sugar: Math.random() >= 0.5,
         energy: randomInteger(70, 500),
         contents: getContents
+        }
       }
-    }
+
+      if(arr.amount > i && arr.amount < 5) {
+        cardsElement.classList.add('card--little');
+        cardsElement.classList.remove('card--in-stock');
+      } else if(arr.amount == 0) {
+        cardsElement.classList.add('card--soon');
+        cardsElement.classList.remove('card--in-stock');
+      };
+
+      cardsElement.querySelector('.card__title').textContent = arr.name;
+      cardsElement.querySelector('.card__price').innerHTML = '{{price}} <span class="card__currency">₽</span><span class="card__weight">/ {{weight}} Г</span>;';
+      cardsElement.querySelector('.star__count').textContent = arr.rating.number;
+
+      cardsElement.querySelector('.stars__rating').classList.remove('stars__rating--five');
+      if(arr.rating.value === 1) {
+        cardsElement.querySelector('.stars__rating').classList.add('stars__rating--one');
+        if(arr.rating.value === 2) {
+          cardsElement.querySelector('.stars__rating').classList.add('stars__rating--two');
+        };
+        if(arr.rating.value === 3) {
+          cardsElement.querySelector('.stars__rating').classList.add('stars__rating--three');
+        };
+        if(arr.rating.value === 4) {
+          cardsElement.querySelector('.stars__rating').classList.add('stars__rating--four');
+        };
+        if(arr.rating.value === 5) {
+          cardsElement.querySelector('.stars__rating').classList.add('stars__rating--five');
+        };
+      }
+
+      if(nutritionFacts.sugar){
+        cardsElement.querySelector('.card__characteristic').textContent = 'Содержит сахар';
+      } else {ardsElement.querySelector('.card__characteristic').textContent = 'Без сахара'};
+      cardsElement.querySelector('.card__composition-list').textContent = arr.contents;
   }
 
-  return arr;
-
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < 26; i++) {
+  fragment.appendChild(getCards(arr[i]));
 }
 
-console.log(getArr());
+
+

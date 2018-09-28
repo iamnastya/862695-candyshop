9a5c39f24d1a167e08aeb5acdd7397f855d43a75
Nastasia <nastya.nty@gmail.com>
2018-09-28 14:59:18 +0500

    goods.js renewed

--- js/goods.js
+++ js/goods.js
@@ -1,139 +1,118 @@
-'use strict'
+'use strict';
 
-var nameArr = ['Чесночные сливки', 'Огуречный педант', 'Молочная хрюша', 'Грибной шейк',
-  'Баклажановое безумие',
-  'Паприколу итальяно',
-  'Нинзя-удар васаби',
-  'Хитрый баклажан',
-  'Горчичный вызов',
-  'Кедровая липучка',
-  'Корманный портвейн',
-  'Чилийский задира',
-  'Беконовый взрыв',
-  'Арахис vs виноград',
-  'Сельдерейная душа',
-  'Початок в бутылке',
-  'Чернющий мистер чеснок',
-  'Раша федераша',
-  'Кислая мина',
-  'Кукурузное утро',
-  'Икорный фуршет',
-  'Новогоднее настроение',
-  'С пивком потянет',
-  'Мисс креветка',
-  'Бесконечный взрыв',
-  'Невинные винные',
-  'Бельгийское пенное',
-  'Острый язычок'];
+// функция рандома и выбора произвольного значения из массива
 
-  var pictureArr = ['img/cards/gum-cedar.jpg', 'img/cards/gum-chile.jpg', 'img/cards/gum-eggplant.jpg', 'img/cards/gum-mustard.jpg']
+function randomInteger(min, max) {
+  var rand = min + Math.random() * (max + 1 - min);
+  rand = Math.floor(rand);
+  return rand;
+}
 
+var getArrayElement = function (array) {
+  var rand = Math.floor(Math.random() * array.length);
+  return array[rand];
+};
 
-  function randomInteger(min, max) {
-    var rand = min + Math.random() * (max + 1 - min);
-    rand = Math.floor(rand);
-    return rand;
+// массивы
+
+var NAMES = ['Чесночные сливки', 'Огуречный педант', 'Молочная хрюша', 'Грибной шейк', 'Баклажановое безумие', 'Паприколу итальяно', 'Нинзя-удар васаби', 'Хитрый баклажан', 'Горчичный вызов', 'Кедровая липучка', 'Корманный портвейн', 'Чилийский задира', 'Беконовый взрыв', 'Арахис vs виноград', 'Сельдерейная душа', 'Початок в бутылке', 'Чернющий мистер чеснок', 'Раша федераша', 'Кислая мина', 'Кукурузное утро', 'Икорный фуршет', 'Новогоднее настроение', 'С пивком потянет', 'Мисс креветка', 'Бесконечный взрыв', 'Невинные винные', 'Бельгийское пенное', 'Острый язычок'];
+var PICTURES = ['img/cards/ice-garlic.jpg', 'img/cards/ice-cucumber.jpg', 'img/cards/ice-pig.jpg', 'img/cards/ice-mushroom.jpg', 'img/cards/ice-eggplant.jpg', 'img/cards/ice-italian.jpg', 'img/cards/gum-wasabi.jpg', 'img/cards/gum-eggplant.jpg', 'img/cards/gum-mustard.jpg', 'img/cards/gum-cedar.jpg', 'img/cards/gum-portwine.jpg', 'img/cards/gum-chile.jpg', 'img/cards/soda-bacon.jpg', 'img/cards/soda-peanut-grapes.jpg', 'img/cards/soda-celery.jpg', 'img/cards/soda-cob.jpg', 'img/cards/soda-garlic.jpg', 'img/cards/soda-russian.jpg', 'img/cards/marmalade-sour.jpg', 'img/cards/marmalade-corn.jpg', 'img/cards/marmalade-caviar.jpg', 'img/cards/marmalade-new-year.jpg', 'img/cards/marmalade-beer.jpg', 'img/cards/marshmallow-shrimp.jpg', 'img/cards/marshmallow-bacon.jpg', 'img/cards/marshmallow-wine.jpg', 'img/cards/marshmallow-beer.jpg', 'img/cards/marshmallow-spicy.jpg'];
+var CONTENTS = ['молоко', 'сливки', 'вода', 'пищевой краситель', 'патока', 'ароматизатор бекона', 'ароматизатор свинца', 'ароматизатор дуба, идентичный натуральному', 'ароматизатор картофеля', 'лимонная кислота', 'загуститель', 'эмульгатор', 'консервант: сорбат калия', 'посолочная смесь: соль, нитрит натрия', 'ксилит', 'карбамид', 'вилларибо', 'виллабаджо'];
+
+var getContents = function () {
+  var randomContents = [];
+  for (var i = 0; i < randomInteger(1, CONTENTS.length); i++) {
+    randomContents.push(getArrayElement(CONTENTS));
   }
+  return randomContents;
+};
 
+// функция, которая засовывает объекты в массив
 
-  var contentsArr = ['молоко',
-    'сливки',
-    'вода',
-    'пищевой краситель',
-    'патока',
-    'ароматизатор бекона',
-    'ароматизатор свинца',
-    'ароматизатор дуба, идентичный натуральному',
-    'ароматизатор картофеля',
-    'лимонная кислота',
-    'загуститель',
-    'эмульгатор',
-    'консервант: сорбат калия',
-    'посолочная смесь: соль, нитрит натрия',
-    'ксилит',
-    'карбамид',
-    'вилларибо',
-    'виллабаджо'];
-
-    var getContents = function () {
-    var contents = "";
-    for (var i = 0; i < randomInteger(1, contentsArr.length-1); i++) {
-      if (i == 0) {
-        contents = contentsArr[randomInteger(0,contentsArr.length-1)];
-      } else {
-        contents = contents + ", " + contentsArr[randomInteger(0,contentsArr.length-1)];
-      }
-    }
-    return contents
+var card = {
+  name: getArrayElement(NAMES),
+  picture: getArrayElement(PICTURES),
+  amount: randomInteger(0, 20),
+  price: randomInteger(100, 1500),
+  weight: randomInteger(30, 300),
+  rating: {
+    value: randomInteger(0, 5),
+    number: randomInteger(10, 900)
+  },
+  nutritionFacts: {
+    sugar: Math.random() >= 0.5,
+    energy: randomInteger(70, 500),
+    contents: getContents()
   }
+};
 
-  var userDialog = document.querySelector('.catalog__cards');
-  userDialog.classList.remove('catalog__cards--load');
-  userDialog.querySelector('.catalog__load').classList.add('visually-hidden');
+var getCards = function (cardsAmount) {
+  var cards = [];
+  for (var i = 0; i < cardsAmount; i++) {
+    cards.push(card);
+  }
+  return cards;
+};
 
-  var cardsTemplate = document.querySelector('#card')
+// убираем catalog__cards--load и еще один класс
+
+var userDialog = document.querySelector('.catalog__cards');
+userDialog.classList.remove('catalog__cards--load');
+userDialog.querySelector('.catalog__load').classList.add('visually-hidden');
+
+// создаем DOM-элементы
+
+var cardTemplate = document.querySelector('#card')
     .content
     .querySelector('.catalog__card');
 
-  var getCards = function (){
-  var cardsElement = cardsTemplate.cloneNode(true);
+var renderCard = function (element) {
+  var cardElement = cardTemplate.cloneNode(true);
 
-    var arr = {
-      name: nameArr[randomInteger(0, nameArr.length - 1)],
-      picture: pictureArr[randomInteger(0, pictureArr.length - 1)],
-      amount: randomInteger(0,20),
-      price: randomInteger(100, 1500),
-      weight: randomInteger(30, 300),
-      rating: {
-        value: randomInteger(1,5),
-        number: randomInteger(10, 900)
-      },
-        nutritionFacts: {
-        sugar: Math.random() >= 0.5,
-        energy: randomInteger(70, 500),
-        contents: getContents
-        }
-      }
-
-      if(arr.amount > i && arr.amount < 5) {
-        cardsElement.classList.add('card--little');
-        cardsElement.classList.remove('card--in-stock');
-      } else if(arr.amount == 0) {
-        cardsElement.classList.add('card--soon');
-        cardsElement.classList.remove('card--in-stock');
-      };
-
-      cardsElement.querySelector('.card__title').textContent = arr.name;
-      cardsElement.querySelector('.card__price').innerHTML = '{{price}} <span class="card__currency">₽</span><span class="card__weight">/ {{weight}} Г</span>;';
-      cardsElement.querySelector('.star__count').textContent = arr.rating.number;
-
-      cardsElement.querySelector('.stars__rating').classList.remove('stars__rating--five');
-      if(arr.rating.value === 1) {
-        cardsElement.querySelector('.stars__rating').classList.add('stars__rating--one');
-        if(arr.rating.value === 2) {
-          cardsElement.querySelector('.stars__rating').classList.add('stars__rating--two');
-        };
-        if(arr.rating.value === 3) {
-          cardsElement.querySelector('.stars__rating').classList.add('stars__rating--three');
-        };
-        if(arr.rating.value === 4) {
-          cardsElement.querySelector('.stars__rating').classList.add('stars__rating--four');
-        };
-        if(arr.rating.value === 5) {
-          cardsElement.querySelector('.stars__rating').classList.add('stars__rating--five');
-        };
-      }
-
-      if(nutritionFacts.sugar){
-        cardsElement.querySelector('.card__characteristic').textContent = 'Содержит сахар';
-      } else {ardsElement.querySelector('.card__characteristic').textContent = 'Без сахара'};
-      cardsElement.querySelector('.card__composition-list').textContent = arr.contents;
+  if (element.amount >= 1 && element.amount <= 5) {
+    cardElement.classList.add('card--little');
+    cardElement.classList.remove('card--in-stock');
+  } else if (element.amount === 0) {
+    cardElement.classList.add('card--soon');
+    cardElement.classList.remove('card--in-stock');
   }
 
+  cardElement.querySelector('.card__title').textContent = element.name;
+
+  cardElement.querySelector('.card__price').innerHTML = element.price + ' ' + '<span class="card__currency">₽</span><span class="card__weight">/ ' + element.weight + ' Г</span>';
+
+  cardElement.querySelector('.stars__rating').classList.remove('stars__rating--five');
+  switch (element.rating.value) {
+    case 1:
+      cardElement.querySelector('.stars__rating').classList.add('stars__rating--one');
+      break;
+    case 2:
+      cardElement.querySelector('.stars__rating').classList.add('stars__rating--two');
+      break;
+    case 3:
+      cardElement.querySelector('.stars__rating').classList.add('stars__rating--three');
+      break;
+    case 4:
+      cardElement.querySelector('.stars__rating').classList.add('stars__rating--four');
+      break;
+    case 5:
+      cardElement.querySelector('.stars__rating').classList.add('stars__rating--five');
+      break;
+  }
+
+  cardElement.querySelector('.star__count').textContent = element.rating.number;
+
+  cardElement.querySelector('.card__img').src = element.picture;
+
+  if (element.nutritionFacts.sugar) {
+    cardElement.querySelector('.card__characteristic').textContent = 'Содержит сахар';
+  } else {
+    cardElement.querySelector('.card__characteristic').textContent = 'Без сахара';
+  }
+  cardElement.querySelector('.card__composition-list').textContent = element.contents;
+
+  return cardElement;
+};
+
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < 26; i++) {
-  fragment.appendChild(getCards(arr[i]));
-}
-
-
 
---
+++ js/goods1.js
@@ -0,0 +1,138 @@
+'use strict';
+
+var NAMES = ['Чесночные сливки', 'Огуречный педант', 'Молочная хрюша', 'Грибной шейк',
+  'Баклажановое безумие',
+  'Паприколу итальяно',
+  'Нинзя-удар васаби',
+  'Хитрый баклажан',
+  'Горчичный вызов',
+  'Кедровая липучка',
+  'Корманный портвейн',
+  'Чилийский задира',
+  'Беконовый взрыв',
+  'Арахис vs виноград',
+  'Сельдерейная душа',
+  'Початок в бутылке',
+  'Чернющий мистер чеснок',
+  'Раша федераша',
+  'Кислая мина',
+  'Кукурузное утро',
+  'Икорный фуршет',
+  'Новогоднее настроение',
+  'С пивком потянет',
+  'Мисс креветка',
+  'Бесконечный взрыв',
+  'Невинные винные',
+  'Бельгийское пенное',
+  'Острый язычок'];
+
+var PICTURES = ['img/cards/gum-cedar.jpg', 'img/cards/gum-chile.jpg', 'img/cards/gum-eggplant.jpg', 'img/cards/gum-mustard.jpg'];
+
+var CONTENTS = ['молоко',
+  'сливки',
+  'вода',
+  'пищевой краситель',
+  'патока',
+  'ароматизатор бекона',
+  'ароматизатор свинца',
+  'ароматизатор дуба, идентичный натуральному',
+  'ароматизатор картофеля',
+  'лимонная кислота',
+  'загуститель',
+  'эмульгатор',
+  'консервант: сорбат калия',
+  'посолочная смесь: соль, нитрит натрия',
+  'ксилит',
+  'карбамид',
+  'вилларибо',
+  'виллабаджо'];
+
+function randomInteger(min, max) {
+  var rand = min + Math.random() * (max + 1 - min);
+  rand = Math.floor(rand);
+  return rand;
+}
+
+var getContents = function () {
+  var contents = '';
+  for (var i = 0; i < randomInteger(1, CONTENTS.length - 1); i++) {
+    if (i === 0) {
+      contents = CONTENTS[randomInteger(0, CONTENTS.length - 1)];
+    } else {
+      contents = contents + ', ' + CONTENTS[randomInteger(0, CONTENTS.length - 1)];
+    }
+  }
+  return contents;
+};
+
+var userDialog = document.querySelector('.catalog__cards');
+userDialog.classList.remove('catalog__cards--load');
+userDialog.querySelector('.catalog__load').classList.add('visually-hidden');
+
+var cardsTemplate = document.querySelector('#card')
+    .content
+    .querySelector('.catalog__card');
+
+var getCards = function () {
+  var cardsElement = cardsTemplate.cloneNode(true);
+
+  var arr = {
+    name: NAMES[randomInteger(0, NAMES.length - 1)],
+    picture: PICTURES[randomInteger(0, PICTURES.length - 1)],
+    amount: randomInteger(0, 20),
+    price: randomInteger(100, 1500),
+    weight: randomInteger(30, 300),
+    rating: {
+      value: randomInteger(1, 5),
+      number: randomInteger(10, 900)
+    },
+    nutritionFacts: {
+      sugar: Math.random() >= 0.5,
+      energy: randomInteger(70, 500),
+      contents: getContents
+    }
+  };
+
+  if (arr.amount > i && arr.amount < 5) {
+    cardsElement.classList.add('card--little');
+    cardsElement.classList.remove('card--in-stock');
+  } else if (arr.amount == 0) {
+    cardsElement.classList.add('card--soon');
+    cardsElement.classList.remove('card--in-stock');
+  }
+
+  cardsElement.querySelector('.card__title').textContent = arr.name;
+  cardsElement.querySelector('.card__price').innerHTML = '{{price}} <span class="card__currency">₽</span><span class="card__weight">/ {{weight}} Г</span>;';
+  cardsElement.querySelector('.star__count').textContent = arr.rating.number;
+
+  cardsElement.querySelector('.stars__rating').classList.remove('stars__rating--five');
+  if (arr.rating.value === 1) {
+    cardsElement.querySelector('.stars__rating').classList.add('stars__rating--one');
+    if (arr.rating.value === 2) {
+      cardsElement.querySelector('.stars__rating').classList.add('stars__rating--two');
+    }
+    if (arr.rating.value === 3) {
+      cardsElement.querySelector('.stars__rating').classList.add('stars__rating--three');
+    }
+    if (arr.rating.value === 4) {
+      cardsElement.querySelector('.stars__rating').classList.add('stars__rating--four');
+    }
+    if (arr.rating.value === 5) {
+      cardsElement.querySelector('.stars__rating').classList.add('stars__rating--five');
+    }
+  }
+
+  if (nutritionFacts.sugar) {
+    cardsElement.querySelector('.card__characteristic').textContent = 'Содержит сахар';
+  } else {
+    ardsElement.querySelector('.card__characteristic').textContent = 'Без сахара'
+    ;}
+  cardsElement.querySelector('.card__composition-list').textContent = arr.contents;
+};
+
+var fragment = document.createDocumentFragment();
+for (var i = 0; i < 26; i++) {
+  fragment.appendChild(getCards());
+}
+
+
